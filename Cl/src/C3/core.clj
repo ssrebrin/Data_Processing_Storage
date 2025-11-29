82d610b8b140c86431f99d7d1e6c14da268fd9c5
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-11-29 11:53:44 +0700

    a

--- Cl/src/C3/core.clj
+++ Cl/src/C3/core.clj
@@ -11,15 +11,33 @@
 (defn par-filter
   [pred coll block-size parallelism]
   (let [blocks (batch block-size coll)]
-    (letfn [(process [blks active-futs]
+    (letfn [(process [blks futs]
               (lazy-seq
                 (when-let [b (seq blks)]
-                  (let [
-                        futs   (concat active-futs (doall (map #(future (filter pred %)) (take parallelism b))))]
+                  (let [futs (if (empty? futs) (vec (doall (map #(future (filter pred %)) (take parallelism b)))) futs)
+                        rest-futs (subvec futs 1)
+                        new-block (first (drop parallelism b))
+                        new-fut   (when new-block (future (filter pred new-block)))]
                     (concat @(first futs)
-                            (process (drop parallelism b) (rest futs)))))))]
-      (process blocks '()))))
+                            (process (rest blks) (if new-fut (conj rest-futs new-fut) rest-futs)))))))]
+      (process blocks []))))
 
+(defn is-prime [n]
+  (cond
+    (< n 2) false
+    (= n 2) true
+    (even? n) false
+    :else (let [sqrt-n (Math/sqrt n)]
+            (not-any? #(zero? (mod n %))
+                      (range 3 (inc (int sqrt-n)) 2)))))
 
 (defn -main [& args]
-    (println (take 50 (par-filter even? (range) 5 10))))
+
+  (time
+    (let [result (take 10000 (par-filter is-prime (range) 100 1000000))]
+      (println "Number of filtered elements:" (count result))))
+    (time
+    (let [result (take 10000 (filter is-prime (range)))]
+      (println "Number of filtered elements:" (count result)))
+)
+)
