68217f78ae1d23e4ba828086f783b9dbbbfc2852
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-12-10 23:22:29 +0700

    c3 c4

--- Cl/src/C3/core.clj
+++ Cl/src/C3/core.clj
@@ -6,38 +6,40 @@
 (defn batch [n coll]
   (lazy-seq
     (when-let [s (seq coll)]
-      (cons (take n s) (batch n (drop n s))))))
+      (cons (doall (take n s))
+            (batch n (drop n s))))))
+
 
 (defn par-filter
   [pred coll block-size parallelism]
-  (let [blocks (batch block-size coll)]
-    (letfn [(process [blks futs]
+
+  (let [blocks (batch block-size coll)
+        f      #(doall (filter pred %))]
+
+    (letfn [(step [[b & bs :as all-blocks] futs]
               (lazy-seq
-                (when-let [b (seq blks)]
-                  (let [futs (if (empty? futs) (vec (doall (map #(future (filter pred %)) (take parallelism b)))) futs)
-                        rest-futs (subvec futs 1)
-                        new-block (first (drop parallelism b))
-                        new-fut   (when new-block (future (filter pred new-block)))]
-                    (concat @(first futs)
-                            (process (rest blks) (if new-fut (conj rest-futs new-fut) rest-futs)))))))]
-      (process blocks []))))
+                (when (seq futs)
+                  (lazy-cat @(first futs)
+                    (let [rest-futs (vec (rest futs))]
+                      (step (rest all-blocks) (if (seq all-blocks)
+                              (conj rest-futs
+                                    (future (f (first all-blocks))))
+                              rest-futs      
+                                    )))))))]
+      (step
+        (drop parallelism blocks)
+        (vec (map #(future (f %))
+                  (take parallelism blocks)))))))
 
-(defn is-prime [n]
-  (cond
-    (< n 2) false
-    (= n 2) true
-    (even? n) false
-    :else (let [sqrt-n (Math/sqrt n)]
-            (not-any? #(zero? (mod n %))
-                      (range 3 (inc (int sqrt-n)) 2)))))
 
-(defn -main [& args]
 
-  (time
-    (let [result (take 10000 (par-filter is-prime (range) 100 1000000))]
-      (println "Number of filtered elements:" (count result))))
-    (time
-    (let [result (take 10000 (filter is-prime (range)))]
-      (println "Number of filtered elements:" (count result)))
-)
-)
+(defn tst [a] (do
+                 (Thread/sleep 10)
+                 (= (mod a 2) 0)))
+
+(defn -main []
+  (do
+    (time (doall (take 1000 (par-filter tst (range) 200 100))))
+    (time (doall (take 1000 (filter tst (range)))))
+    )
+  )
